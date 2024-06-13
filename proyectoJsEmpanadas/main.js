990c79a20c460fc9cee9fcfc44872ab29f33489c
More-Pe <peraltamorena.va@gmail.com>
2024-06-13 20:57:46 +0200

    Basic data added

--- proyectoJsEmpanadas/main.js
+++ proyectoJsEmpanadas/main.js
@@ -1,3 +1,24 @@
+/*
+
+Datos:
+a= cantidad de empanadas de 12 euros
+b= cantidad de empanadas de 14 euros
+c= cantidad de empanadas empanadas de 16 euros
+
+ab= mixtas de 13 euros
+ac= mixtas de 14 euros
+bc= mixtas de 15 euros
+
+x= cantidad total de empanadas (a+b+c)
+
+y= cantidad total de empanadas a pagar (x/3)
+
+*/
+
+
+
+
+
 export const fn = (/* argumentos de la función */) => {
    // Cuerpo de la función: Completa la lógica de la función aquí.
 };
